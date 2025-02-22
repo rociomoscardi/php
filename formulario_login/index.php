605d6285e591d3c855139e1a4a70887f207999a6
rociomoscardi <rocioabrilmoscardi@gmail.com>
2025-02-22 14:18:00 -0300

    cambios 22/02

---
+++ formulario_login/index.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-start py-5">
+                <h1>Formulario</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <form action="" method="post">
+                    <label for=""></label>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
