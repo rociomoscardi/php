c780393764828c32013994a6dfebd17545921229
rociomoscardi <rocioabrilmoscardi@gmail.com>
2025-02-22 12:07:30 -0300

    cambios 22/02

--- empleados.php
+++ empleados.php
@@ -3,27 +3,32 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$aEmpleados = array ();
-$aEmpleados[] = array (
+$aEmpleados = array();
+$aEmpleados[] = array(
     "dni" => 33300123,
     "nombre" => "David García",
     "bruto" => 85000.30
 );
-$aEmpleados[] = array (
+$aEmpleados[] = array(
     "dni" => 40874456,
     "nombre" => "Ana del Valle",
     "bruto" => 90000
 );
-$aEmpleados[] = array (
+$aEmpleados[] = array(
     "dni" => 67567565,
     "nombre" => "Andrés Perez",
     "bruto" => 100000
 );
-$aEmpleados[] = array (
+$aEmpleados[] = array(
     "dni" => 75744545,
     "nombre" => "Victoria Luz",
     "bruto" => 70000
 );
+
+function calcularNeto($bruto)
+{
+    return $bruto - ($bruto * 0.17);
+}
 ?>
 
 <!DOCTYPE html>
@@ -39,7 +44,40 @@ $aEmpleados[] = array (
 <body>
     <main class="container">
         <div class="row">
-            <h1 class="col-12 text-center py-5">Listado de empleados</h1>
-        </div>
+            <div class="col-12 text-center py-5">
+                <h1>Listado de empleados</h1>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <table class="table table-hover border">
+                        <thead>
+                            <tr>
+                                <th>DNI</th>
+                                <th>Nombre</th>
+                                <th>Sueldo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php
+                            foreach ($aEmpleados as $empleado) {
+                            ?>
+                                <tr>
+                                    <td> <?php echo $empleado["dni"] ?></td>
+                                    <td> <?php echo mb_strtoupper($empleado["nombre"]) ?></td>
+                                    <td> $<?php echo number_format(calcularNeto($empleado["bruto"]), 2, ",", ".") ?></td>
+                                </tr>
+
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                       <?php echo "Cantidad de empleados activos: " . count($aEmpleados); ?>
+                    </div>
+                </div>
     </main>
 </body>
