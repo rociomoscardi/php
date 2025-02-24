06f37d241ad7e94be072e5baa54989e5afaf3d3a
rociomoscardi <rocioabrilmoscardi@gmail.com>
2025-02-24 16:55:11 -0300

    cambios 24/02

--- clientes_session.php
+++ clientes_session.php
@@ -3,9 +3,38 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
+if (isset($_SESSION["listadoClientes"])){
+    //si existe la variable de session listado clientes entonces le asigno su contenido a $aClientes.
+    $aClientes = $_SESSION["listadoClientes"];
+} else {
+    $aClientes = array(); //para inicializarlo como un array vacío y que no de error la primera vez.
+}
+
 if ($_POST){
+    //si hace click en Enviar:
+    if (isset ($_POST["btnEnviar"])){
+    //asignamos en variables los datos que vienen del formulario.
     $nombre = $_POST["txtNombre"];
     $dni = $_POST["txtDni"];
+    $telefono = $_POST["txtTelefono"];
+    $edad = $_POST["txtEdad"];
+
+    //creamos un array que contendrá el listado de clientes.
+    $aClientes[] = array ("nombre" => $nombre,
+    "dni" => $dni,
+    "telefono" => $telefono,
+    "edad" => $edad
+    );
+
+    //actualiza el contenido de variable en session.
+    $_SESSION["listadoClientes"] = $aClientes;
+
+    } else (isset ($_POST["btnEliminar"])){
+        session_destroy();
+        $aClientes = array();
+    }
 }
 ?>
 
@@ -28,7 +57,7 @@ if ($_POST){
         </div>
         <div class="row">
             <div class="col-4 offset-1">
-                <form action="" method="$_POST">
+                <form action="" method="post">
                     <div class="pb-3">
                         <label for="txtNombre">Nombre:</label>
                         <input type="text" name="txtNombre" id="txtNombre" placeholder="Ingrese nombre y apellido" class="form-control">
@@ -47,10 +76,10 @@ if ($_POST){
                     </div>
                     <div class="row">
                         <div class="col-2">
-                            <button type="submit" class="btn btn-primary">Enviar</button>
+                            <button type="submit" name="btnEnviar" class="btn btn-primary">Enviar</button>
                         </div>
                         <div class="col-2">
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
+                            <button type="submit" name="btnEliminar" class="btn btn-danger">Eliminar</button>
                         </div>
                     </div>
                 </form>
@@ -66,7 +95,15 @@ if ($_POST){
                         </tr>
                     </thead>
                     <tbody>
-
+                        <?php
+                        foreach ($aClientes as $cliente): ?>
+                        <tr>
+                            <td><?php echo $cliente["nombre"]?></td>
+                            <td><?php echo $cliente["dni"]?></td>
+                            <td><?php echo $cliente["telefono"]?></td>
+                            <td><?php echo $cliente["edad"]?></td>
+                        </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
